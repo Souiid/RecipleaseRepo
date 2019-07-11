9afea62d78a2ed1494b4361106375fa1a2f5e5f6
skurtskurt <idriss.souissi12@gmail.com>
2019-07-11 17:09:31 +0200

    Add entites in context with CoreData

--- Reciplease/Controller/RecipeDescriptionViewController.swift
+++ Reciplease/Controller/RecipeDescriptionViewController.swift
@@ -12,14 +12,19 @@ import SafariServices
 class RecipeDescriptionViewController: UIViewController {
     
     var recipeDescription: RecipeDescriptionDecodable!
-    var recipesIngredients: [String]!
+  
     var recipeService: RecipeService!
+    var recipe: Match!
+    let recipeToSave = Recipe(context: AppDelegate.viewContext)
+
    
     @IBOutlet weak var recipeTitleLabel: UILabel!
     @IBOutlet weak var recipeDetailTextView: UITextView!
     
     @IBOutlet weak var recipeIngredientsLabel: UILabel!
     @IBOutlet weak var recipeImage: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +34,15 @@ class RecipeDescriptionViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         recipeTitleLabel.text = recipeDescription.name
-        recipeIngredientsLabel.text = recipesIngredients.joined(separator: ", ")
+        recipeIngredientsLabel.text = recipe.ingredients.joined(separator: ", ")
         recipeDetailTextView.text = recipeDescription.ingredientLines.joined(separator: "\n")
         let stringURL = recipeDescription.images[0].hostedLargeURL
-        print("recipeDescriptionImage: ", stringURL)
+      
         RecipeService().downloadImage(with: stringURL) { (data) in
             guard let data = data else {return}
             self.recipeImage.image = UIImage(data: data)
+            self.recipeToSave.image = data
+         
         }
         
     }
@@ -45,13 +52,24 @@ class RecipeDescriptionViewController: UIViewController {
     
     }
     
-    
-    @IBAction func dismiss() {
-        navigationController?.popViewController(animated: true)
+    @IBAction func addToFavorite() {
+        //let recipeToSave = Recipe(context: AppDelegate.viewContext)
+       
+        recipeToSave.name = self.recipe.recipeName
+        recipeToSave.rate = String(self.recipe.rating)
+        recipeToSave.totalTime = String(self.recipe.totalTimeInSeconds)
+        
+        for ingredient in self.recipe.ingredients {
+            let ingredientEntity = Ingredient(context: AppDelegate.viewContext)
+            ingredientEntity.name = ingredient
+            ingredientEntity.recipe = recipeToSave
+        }
+        
+        try? AppDelegate.viewContext.save()
+        
     }
     
-    
-    func getRecipeDetailsOnSafari(with stringUrl: String) {
+  func getRecipeDetailsOnSafari(with stringUrl: String) {
         guard let url = URL(string: stringUrl) else {
             
             return
