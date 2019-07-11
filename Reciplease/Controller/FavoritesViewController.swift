287b010453038b0efada1d0626a4f6434d262c89
skurtskurt <idriss.souissi12@gmail.com>
2019-07-11 17:12:15 +0200

    Get entites (fetchAll) with CoreData and performSegue to pass informations in FavoriteDescriptionVC

--- Reciplease/Controller/FavoritesViewController.swift
+++ Reciplease/Controller/FavoritesViewController.swift
@@ -9,22 +9,60 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-
+    var recipes = Recipe.fetchAll()
+    var index = 0
+    
+    @IBOutlet weak var favoritesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        favoritesTableView.register(UINib.init(nibName: "RecipeTableViewCell", bundle: nil), forCellReuseIdentifier: "RecipeTableViewCell")
+        print(Recipe.fetchAll())
+     
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        recipes = Recipe.fetchAll()
+        favoritesTableView.reloadData()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "segueToFavDescriptionVC" {
+            
+            if let favDescriptionVC = segue.destination as? FavoriteDescriptionViewController {
+                // Passer le titre, les ingredients, l'image, ingredientline
+                
+                favDescriptionVC.recipe = recipes[index]
+                
+              //  guard let recipeDescription = recipeDescription else {return}
+                //recipeDescriptionVC.recipeDescription = recipeDescription
+               // recipeDescriptionVC.recipe = recipes[index]
+            }
+        }
     }
-    */
 
+}
+
+extension FavoritesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = favoritesTableView.dequeueReusableCell(withIdentifier: "RecipeTableViewCell", for: indexPath) as? RecipeTableViewCell else {return UITableViewCell()}
+        
+        let recipe = recipes[indexPath.row]
+        cell.recipe = recipe
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let recipe = recipes[indexPath.row]
+        index = indexPath.row
+        performSegue(withIdentifier: "segueToFavDescriptionVC", sender: self)
+    }
+
+    
 }
