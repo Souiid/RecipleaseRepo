058080a48d2ef8dfa9c2eaab98b2c601768317d0
skurtskurt <idriss.souissi12@gmail.com>
2019-07-11 17:07:33 +0200

    Add NSManagedObject/ function delete

--- Reciplease/Model/Recipe.swift
+++ Reciplease/Model/Recipe.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import CoreData
+
+class Recipe: NSManagedObject {
+    static func fetchAll(viewContext: NSManagedObjectContext = AppDelegate.viewContext) -> [Recipe] {
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        guard let recipeList = try? viewContext.fetch(request) else {return []}
+        return recipeList
+    }
+    
+    static func deleteRecipe(recipe: Recipe, viewContext: NSManagedObjectContext = AppDelegate.viewContext) {
+        //Recipe.fetchAll(viewContext: viewContext).startIndex({viewContext.delete($0)})
+  
+        viewContext.delete(recipe)
+        
+        
+       // Recipe.fetchAll(viewContext: viewContext).forEach({ viewContext.delete($0) })
+        try? viewContext.save()
+    }
+}
+
