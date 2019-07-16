fa4578e0cf10872903f9ee086a941697b612d376
skurtskurt <idriss.souissi12@gmail.com>
2019-07-16 17:56:01 +0200

    Remove fetchAll

--- Reciplease/Model/Ingredient.swift
+++ Reciplease/Model/Ingredient.swift
@@ -10,11 +10,7 @@ import Foundation
 import CoreData
 
 class Ingredient: NSManagedObject {
-    static func fetchAll(viewContext: NSManagedObjectContext = AppDelegate.viewContext) -> [Ingredient] {
-        let request: NSFetchRequest<Ingredient> = Ingredient.fetchRequest() 
-        guard let ingredientList = try? viewContext.fetch(request) else {return []}
-        return ingredientList
-    }
+  
 }
 
 
