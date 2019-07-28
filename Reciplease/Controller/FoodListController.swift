492ac1c7a5218fcef8d7dd191645d87c8803e68f
skurtskurt <idriss.souissi12@gmail.com>
2019-07-28 21:33:39 +0200

    Add outlets / Modification of ViewDidLoad / Add viewWillAppear/ Modification of action deleteAll, Add activityIndicator / Labels / dismissKeyboard / Modification of viewForFooterSection

--- Reciplease/Controller/FoodListController.swift
+++ Reciplease/Controller/FoodListController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
-import CoreData
+
 
 class FoodViewController: UIViewController {
+    
+    //MARK: - Outlets / propreties / viewDidLoad
 
     @IBOutlet weak var foodTableView: UITableView!
     @IBOutlet weak var inputFoodTextField: UITextField!
+    @IBOutlet weak var yourIngredientsLabel: UILabel!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var searchButton: UIButton!
     
     let foodService = FoodService()
     var food = Food(name: "")
@@ -23,9 +29,20 @@ class FoodViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        yourIngredientsLabel.isHidden = true
+        clearButton.isHidden = true
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        activityIndicator.isHidden = true
+        searchButton.isHidden = false
+        activityIndicator.stopAnimating()
+    }
     
+    //MARK: - Actions
+    
+    // Append food in List
     @IBAction func appendFoodToList() {
        
         guard let foodName = inputFoodTextField.text else {return}
@@ -35,29 +52,38 @@ class FoodViewController: UIViewController {
         inputFoodTextField.text = ""
     }
     
+    // Delete all food in list
     @IBAction func deleteAll() {
         foodService.deleteAll()
+        foodNameArray.removeAll()
         foodTableView.reloadData()
     }
     
-    
+    // Get all recipes according user's selection
     @IBAction func showRecipes() {
-  
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        searchButton.isHidden = true
         for food in foodService.foodArray {
             foodNameArray.append(food.name)
         }
         
         let textTosearch = foodNameArray.joined(separator: "&allowedIngredient%5B%5D=")
-        
         yummlyService.getRecipes(textTosearch: textTosearch) { (success, recipes) in
-           
-            if success {
+            
+           if success {
                 self.recipes = recipes
                 self.performSegue(withIdentifier: "segueToRecipesList", sender: self)
+            
             }
         }
+        
+//        activityIndicator.isHidden = true
+//        searchButton.isHidden = false
+//        activityIndicator.stopAnimating()
     }
     
+    //To come back in this view controller from RecipeDescription
     @IBAction func unwindToFoodList(segue:UIStoryboardSegue) { }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,12 +91,20 @@ class FoodViewController: UIViewController {
             
             if let recipesVC = segue.destination as? RecipesViewController {
                 guard let recipes = recipes else {return}
+                print(recipes)
                 recipesVC.recipes = recipes
             }
         }
     }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        
+        inputFoodTextField.resignFirstResponder()
+    }
+    
 }
 
+// All tableview methods 
 extension FoodViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foodService.foodArray.count
@@ -90,8 +124,12 @@ extension FoodViewController: UITableViewDataSource, UITableViewDelegate {
             label.text = "Add foods in list"
             label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
             label.textAlignment = .center
-            label.textColor = .darkGray
-            //return label
+            label.textColor = .white
+            yourIngredientsLabel.isHidden = true
+            clearButton.isHidden = true
+        }else {
+            yourIngredientsLabel.isHidden = false
+            clearButton.isHidden = false
         }
         return label
     }
