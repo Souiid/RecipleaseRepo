5c4c5ccc08edc6ccd02c84011c099051dab00062
skurtskurt <idriss.souissi12@gmail.com>
2019-07-28 21:45:20 +0200

    Add activityIndicator, viawWillAppear, viewForFooterInSection / Modification of didSelectRowAt indexPath

--- Reciplease/Controller/RecipesViewController.swift
+++ Reciplease/Controller/RecipesViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 
 class RecipesViewController: UIViewController {
     
+    //MARK: - Outlets / propreties / viewDidLoad
+    
     @IBOutlet weak var recipeImageView: UIImageView!
     
     @IBOutlet weak var recipesTableView: UITableView!
    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var index = 0
     
@@ -23,32 +26,39 @@ class RecipesViewController: UIViewController {
     var image = UIImage()
     var recipeTableviewCell = RecipeTableViewCell()
   
-    var recipeDescription: RecipeDescriptionDecodable!
+    var recipeDescription: RecipeDescriptionDecodable?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
         recipesTableView.register(UINib.init(nibName: "RecipeTableViewCell", bundle: nil), forCellReuseIdentifier: "RecipeTableViewCell")
-        print(recipes)
-        
-      
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        recipesTableView.isHidden = false
+        self.activityIndicator.isHidden = true
+        self.activityIndicator.stopAnimating()
+    }
+    
+    
+    
+    //Prepare informations to the next view controller
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segueToRecipeDescription" {
             
             if let recipeDescriptionVC = segue.destination as? RecipeDescriptionViewController {
                 guard let recipeDescription = recipeDescription else {return}
                 recipeDescriptionVC.recipeDescription = recipeDescription
-                recipeDescriptionVC.recipesIngredients = recipes[index].ingredients
-                print("INDEX : ", index)
+                recipeDescriptionVC.recipe = recipes[index]
+                recipeDescriptionVC.favoris = false
             }
         }
     }
-
 }
-    
+
+ //All tableview methods
 extension RecipesViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -65,16 +75,35 @@ extension RecipesViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        recipesTableView.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
         let recipe = recipes[indexPath.row]
         index = indexPath.row
         let id = recipe.id
         YummlyService().getRecipeDescription(id: id) { (success, recipeDescription) in
+            
             if success {
                 self.recipeDescription = recipeDescription
-                print("DESCRIPTION : ", self.recipeDescription)
                 self.performSegue(withIdentifier: "segueToRecipeDescription", sender: self)
+            }else {
+                self.recipesTableView.isHidden = false
+                self.activityIndicator.isHidden = true
+                self.activityIndicator.stopAnimating()
             }
         }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let label = UILabel()
+        if recipes.isEmpty {
+        
+            label.text = "The search not success"
+            label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
+            label.textAlignment = .center
+            label.textColor = .darkGray
+        }
+        return label
     }
 }
 
