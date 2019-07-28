a3ece3b6d38c10612a656e80b432d8790dcaea67
skurtskurt <idriss.souissi12@gmail.com>
2019-07-28 21:52:29 +0200

    Add methods

--- Reciplease/Model/Recipe.swift
+++ Reciplease/Model/Recipe.swift
@@ -10,25 +10,23 @@ import Foundation
 import CoreData
 
 class Recipe: NSManagedObject {
+    // Request All recipes in viewContext
     static func fetchAll(viewContext: NSManagedObjectContext = AppDelegate.viewContext) -> [Recipe] {
         let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
         guard let recipeList = try? viewContext.fetch(request) else {return []}
         return recipeList
     }
     
+    //Delete one recipe from viewContext
     static func deleteRecipe(recipe: Recipe, viewContext: NSManagedObjectContext = AppDelegate.viewContext) {
-        //Recipe.fetchAll(viewContext: viewContext).startIndex({viewContext.delete($0)})
-  
         viewContext.delete(recipe)
-        
-        
-       // Recipe.fetchAll(viewContext: viewContext).forEach({ viewContext.delete($0) })
         try? viewContext.save()
     }
     
-    static func isRegistered(name: String, viewContext: NSManagedObjectContext = AppDelegate.viewContext)-> Bool {
+    // Verify if a recipe is registered
+    static func isRegistered(id: String, viewContext: NSManagedObjectContext = AppDelegate.viewContext)-> Bool {
         let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
-        request.predicate = NSPredicate(format: "name = %@", name)
+        request.predicate = NSPredicate(format: "id = %@", id)
         guard let recipeList = try? viewContext.fetch(request) else {return false}
         if recipeList.isEmpty {
             return false
@@ -36,5 +34,31 @@ class Recipe: NSManagedObject {
         return true
         
     }
+    // Search a recipe by name
+    static func search(name: String, viewContext: NSManagedObjectContext = AppDelegate.viewContext) -> [Recipe]{
+       
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        request.predicate = NSPredicate(format: "name contains[cd] %@", name)
+        guard let recipeList = try? viewContext.fetch(request) else {return []}
+        if recipeList.isEmpty {
+            return []
+        }
+        return recipeList
+    }
+    
+    static func deleteRecipe(id: String, viewContext: NSManagedObjectContext = AppDelegate.viewContext) {
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        request.predicate = NSPredicate(format: "id = %@", id)
+        guard let recipeList = try? viewContext.fetch(request) else {return}
+        guard let recipe = recipeList.first else {return}
+        viewContext.delete(recipe)
+        try? viewContext.save()
+    }
+    
+//    func createRecipe(viewContext: NSManagedObjectContext = AppDelegate.viewContext)-> Recipe{
+//        let recipe = Recipe(context: viewContext)
+//        return recipe
+//        
+//    }
 }
 
